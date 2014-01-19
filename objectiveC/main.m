4727a575dc9a55b641d36c38121a87894bb0a82a
marcvthooft <marcvthooft@gmail.com>
2014-01-19 18:38:58 +0100

    Initial Commit

---
+++ objectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  objectiveC
+//
+//  Created by Marc van 't Hooft on 19-01-14.
+//  Copyright (c) 2014 Epesi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
